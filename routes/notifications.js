26a3ba92b9e663f385ad7ab470d4ca3ac3e0c1a7
AftabUfaq <engr.aftabufaq@gmail.com>
2021-11-17 01:46:06 +0500

    gett doctorbooking , get customer booking

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const admin = require("firebase-admin");
+const  moment = require('moment')
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 const serviceAccount = require('../config/adminsdk.json');
@@ -93,7 +94,102 @@ initializeApp({credential: cert(serviceAccount)});
         console.log("Error sending message:", error);
     });
 
-  res.send(`respond with a resource ss ${bookingid}`);
-});
+      res.send(`respond with a resource ss ${bookingid}`);
+  });
 
+  router.post("/getslots", async function(req, res, next){
+    const db = getFirestore();
+    const date = req.body.date
+    const doctorid = req.body.doctorid
+    const start_time = req.body.start_time
+    const end_time = req.body.end_time
+    const slot_duration  = req.body.slot_duration
+    const bookingsdata = []
+    const allbooking = await db.collection("osb_booking").where("APPOINTMENT_DATE", "==", `${date}`).get()
+      allbooking.docs.map((item) => {
+      let dd = item.data()
+      if(dd.APPOINTMENT_DOCTOR_ID == doctorid){
+        bookingsdata.push(item.data())
+      }
+    })
+
+    const doctor_start_time = new Date(`${date} ` + start_time).getHours();
+    const doctor_end_time = new Date(`${date} ` + end_time).getHours();
+    const time_slots =  await getTimeStops(doctor_start_time,doctor_end_time,slot_duration)
+    const exact_slots =  finddislableslots(time_slots, bookingsdata, date, slot_duration )
+    res.send({'status':true, time_slots:time_slots , exact_slots:exact_slots});
+  })
+
+
+  router.post("/getcustomerbooking", async function(req, res, next){
+    const db = getFirestore();
+
+    const doctorid = req.body.customerid
+    let bookingsdata = []
+
+    const allbooking = await db.collection("osb_booking").where("APPOINTMENT_PATIENT_ID", "==", `${doctorid}`).get()
+    allbooking.docs.map((item) => {
+
+      bookingsdata.push(item.data())
+    })
+    res.send({'status':true, bookings:bookingsdata});
+  })
+
+
+  router.post("/getdoctorbooking", async function(req, res, next){
+    const db = getFirestore();
+    const doctorid = req.body.doctorid
+    let bookingsdata = []
+    const allbooking = await db.collection("osb_booking").where("APPOINTMENT_DOCTOR_ID", "==", `${doctorid}`).get()
+    allbooking.docs.map((item) => {
+      bookingsdata.push(item.data())
+    })
+    res.send({'status':true, bookings:bookingsdata});
+  })
+
+  async function getTimeStops(start,end,slotitme){
+    var startTime = moment(start, 'HH:mm');
+    var endTime = moment(end, 'HH:mm');
+    if(endTime.isBefore(startTime) ){
+      endTime.add(1, 'day');
+    }
+
+    var timeStops = [];
+        while(startTime <= endTime){
+            timeStops.push({time:moment(startTime).format('HH:mm'), avaliability:true});
+            startTime.add(slotitme, 'minutes');
+            startTime = moment(startTime, 'HH:mm')
+    }
+    return timeStops;
+  }
+
+   function  finddislableslots(time_slots, bookingsdata, date, slot_duration){
+    const newslots = []
+      time_slots.forEach(async (element , index) => {
+        const dd = await getslotalaliability(element, bookingsdata, date, slot_duration)
+        newslots.push({...element, avaliability:dd})
+      });
+    return newslots
+  }
+
+  async function getslotalaliability(slot,bookingsdata, date, slot_duration){
+    let slot_time = moment(new Date(`${date} ` + slot.time)).format("x")
+    let slot_end_time =  moment(new Date(`${date} ` + slot.time)).add(`${slot_duration}`, "minutes").format("x")
+    return new Promise((resolve, reject) => {
+        try{
+          bookingsdata.forEach((element, index, array) => {
+                let booking_time = moment(new Date(`${date} ` + element.START_TIME)).format("x")
+                let booking_end_time =  moment(new Date(`${date} ` + element.START_TIME)).add(`${element.APPOINTMENT_TOTAL_TIME}`, "minutes").format("x")
+                if((slot_time >= booking_time && slot_time <=  booking_end_time) || (slot_end_time  >= booking_end_time && slot_end_time <= booking_end_time)){
+                  resolve(false);
+                }
+                if(index === array.length -1){
+                    resolve(true);
+                }
+            });
+        }catch(err){
+            reject(err)
+        }
+    })
+}
 module.exports = router;
